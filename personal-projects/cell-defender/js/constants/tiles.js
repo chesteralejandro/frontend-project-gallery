fe366524bb35fd1be395b0c07cec339cdf31c1a5
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-04-03 04:43:22 +0800

    feat(cell-defender): add first map

---
+++ personal-projects/cell-defender/js/constants/tiles.js
@@ -0,0 +1,7 @@
+const TILES = {
+	0: '<article class="tiles empty">&nbsp;</article>',
+	1: '<article class="tiles wall">&nbsp;</article>',
+	2: '<article class="tiles pill">&nbsp;</article>',
+};
+
+export default TILES;
--- personal-projects/cell-defender/js/main.js
+++ personal-projects/cell-defender/js/main.js
@@ -1,5 +1,9 @@
+import { MAP1 } from './constants/maps.js';
+import TILES from './constants/tiles.js';
+
 const gameScreen = document.getElementById('game-screen');
 const startScreen = document.getElementById('start-screen');
+const map = document.getElementById('map');
 
 window.addEventListener('keyup', (e) => {
 	if (e.code !== 'Enter') return;
@@ -7,3 +11,18 @@ window.addEventListener('keyup', (e) => {
 	startScreen.classList.add('hidden');
 	gameScreen.classList.remove('hidden');
 });
+
+function drawMap() {
+	let tilesString = '';
+
+	for (const arr of MAP1) {
+		for (const tile of arr) {
+			tilesString += TILES[tile];
+		}
+		tilesString += '<br />';
+	}
+
+	map.innerHTML = tilesString;
+}
+
+drawMap();
