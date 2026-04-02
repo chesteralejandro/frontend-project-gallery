7241094b8b30bf66cd280e4d3c6916b4b0241fe8
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-04-03 02:53:43 +0800

    feat(cell-defender): add game screen

--- personal-projects/cell-defender/js/main.js
+++ personal-projects/cell-defender/js/main.js
@@ -1,1 +1,9 @@
-console.log('Start Game.');
+const gameScreen = document.getElementById('game-screen');
+const startScreen = document.getElementById('start-screen');
+
+window.addEventListener('keyup', (e) => {
+	if (e.code !== 'Enter') return;
+
+	startScreen.classList.add('hidden');
+	gameScreen.classList.remove('hidden');
+});
