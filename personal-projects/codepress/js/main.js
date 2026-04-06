9fbc49d4174ea8dd0ba49e447a9272d926e35748
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-04-06 19:45:46 +0800

    feat(codepress): implement HTML minification

--- personal-projects/codepress/js/main.js
+++ personal-projects/codepress/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+const inputTextArea = document.getElementById('input-textarea');
+const outputTextArea = document.getElementById('output-textarea');
+const btnMinify = document.getElementById('btn-minify');
+
+btnMinify.addEventListener('click', () => {
+	let code = inputTextArea.value;
+
+	// 1. Remove HTML comments
+	code = code.replace(/<!--[\s\S]*?-->/g, '');
+
+	// 2. Remove line breaks
+	code = code.replace(/\n/g, '');
+
+	// 3. Collapse multiple spaces into one
+	code = code.replace(/\s{2,}/g, ' ');
+
+	// 4. Remove spaces between tags
+	code = code.replace(/>\s+</g, '><');
+
+	// 5. Trim start and end
+	code = code.trim();
+
+	outputTextArea.value = code;
+});
