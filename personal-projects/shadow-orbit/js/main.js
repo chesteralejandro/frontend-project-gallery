2ca987b26c14f984327b40aad444d8f8338de9c3
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-05-02 03:56:07 +0800

    feat(shadow-orbit): create game class

--- personal-projects/shadow-orbit/js/main.js
+++ personal-projects/shadow-orbit/js/main.js
@@ -1,22 +1,20 @@
 import getElements from './constants/elements.js';
 
+import Game from './systems/game.js';
 import controller from './systems/controller.js';
 import renderer from './systems/renderer.js';
 
 import player from './entities/player.js';
 
 const ELEMENTS = getElements();
+const canvas = ELEMENTS.GAME_CANVAS;
 const ctx = ELEMENTS.GAME_CANVAS.getContext('2d');
-
-let currentState = 'menu';
+const game = new Game(canvas, ctx, player, controller, renderer);
 
 function startGame() {
-	currentState = 'playing';
-
 	showGameScreen();
-	resizeCanvas();
-	gameLoop();
-	player.init(ELEMENTS.GAME_CANVAS);
+	game.resize();
+	game.start();
 }
 
 function showGameScreen() {
@@ -32,6 +30,7 @@ function showMenu() {
 function openPanel() {
 	ELEMENTS.PANEL_OVERLAY.classList.remove('hidden');
 }
+
 function closePanel() {
 	ELEMENTS.PANEL_OVERLAY.classList.add('hidden');
 }
@@ -41,26 +40,8 @@ function closeGame() {
 	console.log('Shutting down...');
 }
 
-function gameLoop() {
-	const canvas = ELEMENTS.GAME_CANVAS;
-
-	renderer.clearGameScreen(ctx, canvas);
-	player.update(canvas, controller);
-	renderer.drawPlayer(ctx, player);
-
-	requestAnimationFrame(gameLoop);
-}
-
-function resizeCanvas() {
-	const container = ELEMENTS.GAME_CANVAS.parentElement;
-
-	ELEMENTS.GAME_CANVAS.width = container.clientWidth;
-	ELEMENTS.GAME_CANVAS.height = container.clientHeight;
-}
-
 ELEMENTS.BUTTONS.START_GAME.addEventListener('click', startGame);
 ELEMENTS.BUTTONS.HOW_TO.addEventListener('click', openPanel);
 ELEMENTS.BUTTONS.CLOSE_PANEL.addEventListener('click', closePanel);
 ELEMENTS.BUTTONS.EXIT_GAME.addEventListener('click', closeGame);
-
-window.addEventListener('resize', resizeCanvas);
+window.addEventListener('resize', () => game.resize());
---
+++ personal-projects/shadow-orbit/js/systems/game.js
@@ -0,0 +1,43 @@
+class Game {
+	constructor(canvas, ctx, player, controller, renderer) {
+		this.currentState = 'menu';
+		this.isRunning = false;
+		this.canvas = canvas;
+		this.ctx = ctx;
+		this.player = player;
+		this.controller = controller;
+		this.renderer = renderer;
+	}
+
+	start() {
+		if (this.isRunning) return;
+
+		this.currentState = 'playing';
+		this.isRunning = true;
+
+		this.player.init(this.canvas);
+
+		this.loop();
+	}
+
+	resize() {
+		const container = this.canvas.parentElement;
+		this.canvas.width = container.clientWidth;
+		this.canvas.height = container.clientHeight;
+	}
+
+	loop() {
+		if (!this.isRunning) return;
+
+		this.renderer.clearGameScreen(this.ctx, this.canvas);
+
+		this.player.update(this.canvas, this.controller);
+
+		const { x, y, size } = this.player;
+		this.renderer.drawPlayer(this.ctx, { x, y, size });
+
+		requestAnimationFrame(() => this.loop());
+	}
+}
+
+export default Game;
