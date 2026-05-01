8b9574967b70dff456c7ff0a3fee0e627bbbb251
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-05-02 00:15:44 +0800

    refactor(shadow-orbit): create functions for listeners

--- personal-projects/shadow-orbit/js/main.js
+++ personal-projects/shadow-orbit/js/main.js
@@ -1,17 +1,20 @@
-import ELEMENTS from '../js/constants/elements.js';
+import ELEMENTS from './constants/elements.js';
 
-ELEMENTS.BUTTONS.START_GAME.addEventListener('click', () => {
+let currentState = 'menu';
+
+function startGame() {
+	currentState = 'playing';
 	console.log('Start mission');
-});
+}
 
-ELEMENTS.BUTTONS.EXIT_GAME.addEventListener('click', () => {
-	window.close();
-});
+function openPanel() {
+	ELEMENTS.PANEL_OVERLAY.classList.remove('hidden');
+}
+function closePanel() {
+	ELEMENTS.PANEL_OVERLAY.classList.add('hidden');
+}
 
-ELEMENTS.BUTTONS.HOW_TO.addEventListener('click', () =>
-	ELEMENTS.PANEL_OVERLAY.classList.remove('hidden'),
-);
-
-ELEMENTS.BUTTONS.CLOSE_PANEL.addEventListener('click', () =>
-	ELEMENTS.PANEL_OVERLAY.classList.add('hidden'),
-);
+ELEMENTS.BUTTONS.START_GAME.addEventListener('click', startGame);
+ELEMENTS.BUTTONS.HOW_TO.addEventListener('click', openPanel);
+ELEMENTS.BUTTONS.CLOSE_PANEL.addEventListener('click', closePanel);
+ELEMENTS.BUTTONS.EXIT_GAME.addEventListener('click', () => window.close());
