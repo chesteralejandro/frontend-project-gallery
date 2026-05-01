671ae12d9566ba1b35f5b4002d78140e8931c188
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-05-02 00:35:32 +0800

    feat(shadow-orbit): add game screen

--- personal-projects/shadow-orbit/js/constants/elements.js
+++ personal-projects/shadow-orbit/js/constants/elements.js
@@ -1,11 +1,14 @@
-const ELEMENTS = {
+const getElements = () => ({
+	SPLASH_SCREEN: document.querySelector('.splash-screen'),
+	GAME_SCREEN: document.getElementById('game-screen'),
 	PANEL_OVERLAY: document.getElementById('panel-screen-overlay'),
+
 	BUTTONS: {
 		START_GAME: document.getElementById('btn-start-game'),
 		HOW_TO: document.getElementById('btn-how-to'),
 		EXIT_GAME: document.getElementById('btn-exit-game'),
 		CLOSE_PANEL: document.getElementById('btn-close-panel'),
 	},
-};
+});
 
-export default ELEMENTS;
+export default getElements;
--- personal-projects/shadow-orbit/js/main.js
+++ personal-projects/shadow-orbit/js/main.js
@@ -1,10 +1,21 @@
-import ELEMENTS from './constants/elements.js';
+import getElements from './constants/elements.js';
 
+const ELEMENTS = getElements();
 let currentState = 'menu';
 
 function startGame() {
 	currentState = 'playing';
-	console.log('Start mission');
+	showGameScreen();
+}
+
+function showGameScreen() {
+	ELEMENTS.SPLASH_SCREEN.classList.add('hidden');
+	ELEMENTS.GAME_SCREEN.classList.remove('hidden');
+}
+
+function showMenu() {
+	ELEMENTS.GAME_SCREEN.classList.add('hidden');
+	ELEMENTS.SPLASH_SCREEN.classList.remove('hidden');
 }
 
 function openPanel() {
@@ -14,7 +25,12 @@ function closePanel() {
 	ELEMENTS.PANEL_OVERLAY.classList.add('hidden');
 }
 
+function closeGame() {
+	// window.close();
+	console.log('Shutting down...');
+}
+
 ELEMENTS.BUTTONS.START_GAME.addEventListener('click', startGame);
 ELEMENTS.BUTTONS.HOW_TO.addEventListener('click', openPanel);
 ELEMENTS.BUTTONS.CLOSE_PANEL.addEventListener('click', closePanel);
-ELEMENTS.BUTTONS.EXIT_GAME.addEventListener('click', () => window.close());
+ELEMENTS.BUTTONS.EXIT_GAME.addEventListener('click', closeGame);
